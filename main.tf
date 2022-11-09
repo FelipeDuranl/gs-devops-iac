71bcc30d78d873a948ff487ac4e8b4d27bb95af4
felipe duran <felipe.maciel@ik.com.br>
2022-11-09 20:36:35 -0300

    revert commit the last update

--- main.tf
+++ main.tf
@@ -24,12 +24,4 @@ module "VPC" {
   source  = "./Modules/VPC"
 }
 
-module "RDS" {
-  source  = "./Modules/RDS"
-  sn_vpc_iac_pub2_1a_id = "${module.VPC.sn_vpc_iac_pub2_1a_id}"
-  sn_vpc_iac_pub2_1b_id = "${module.VPC.sn_vpc_iac_pub2_1b_id}"
-  vpc_iac_security_group_priv_id = "${module.VPC.vpc_iac_security_group_priv_id}"
-
-}  
-
 
