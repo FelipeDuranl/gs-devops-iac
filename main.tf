bf02751de10a183b82fc1f11e4eb43d01e0828ff
felipe duran <felipe.maciel@ik.com.br>
2022-11-09 21:49:39 -0300

    ec2 lb e at

--- main.tf
+++ main.tf
@@ -26,4 +26,16 @@ module "network" {
   source  = "./Modules/VPC"
 }
 
+module "CAMADA1" {
+  source  = "./Modules/CAMADA1"
+  vpc_id = "${module.VPC.vpc_iac_id}"
+  # rds_endpoint = "${module.RDS.rds_endpoint}"
+  # rds_name = "${var.rds_name}"
+  # rds_user = "${var.rds_user}"
+  # rds_password = "${var.rds_password}"
+  sn_vpc_iac_pub_1a_id = "${module.VPC.sn_vpc_iac_pub_1a_id}"
+  sn_vpc_iac_pub_1b_id = "${module.VPC.sn_vpc_iac_pub_1b_id}"
+  vpc_iac_security_group_pub_id = "${module.VPC.vpc_iac_security_group_pub_id}"
+}
+
 
