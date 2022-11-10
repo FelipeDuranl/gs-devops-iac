94bdd0280479d9cab04f7c692b0ddae301bf7559
felipe duran <felipe.maciel@ik.com.br>
2022-11-09 21:19:24 -0300

    update vpc

--- Modules/VPC/main.tf
+++ Modules/VPC/main.tf
@@ -1,254 +1,149 @@
-# VPC
+# RESOURCE: VPC
 
-resource "aws_vpc" "vpc_iac" {
-  cidr_block           = "${var.vpc_iac_cidr}"
-  enable_dns_hostnames = "${var.vpc_iac_dns_hostname}"
-  enable_dns_support =  "${var.vpc_iac_dns_support}"
-
-  tags = {
-    "Name" = "vpc-iac"
-  }
-}
-
-# Internet Gateway
-resource "aws_internet_gateway" "igw_vpc_iac" {
-  vpc_id = aws_vpc.vpc_iac.id
-
-  tags = {
-    "Name" = "igw_vpc_iac"
-  }
+resource "aws_vpc" "vpc" {
+    cidr_block           = "${var.vpc_cidr}"
+    enable_dns_hostnames = "${var.vpc_dns_hostnames}"
 }
 
 
+# RESOURCE: INTERNET GATEWAY
+
+resource "aws_internet_gateway" "igw" {
+    vpc_id = aws_vpc.vpc.id
+}
 
 
+# RESOURCE: SUBNETS
 
-# Tabela de roteamento Pública
+resource "aws_subnet" "sn_aws1_pub" {
+    vpc_id                  = aws_vpc.vpc.id
+    availability_zone       = "${var.vpc_sn_aws1}"
+    cidr_block              = "${var.vpc_sn_aws1_pub_cidr}"
+    map_public_ip_on_launch = "${var.vpc_sn_pub_map_public_ip_on_launch}"
+}
 
-resource "aws_route_table" "vpc_iac_route_table_pub" {
-    vpc_id = aws_vpc.vpc_iac.id
+resource "aws_subnet" "sn_aws2_pub" {
+    vpc_id                  = aws_vpc.vpc.id
+    availability_zone       = "${var.vpc_sn_aws2}"
+    cidr_block              = "${var.vpc_sn_aws2_pub_cidr}"
+    map_public_ip_on_launch = "${var.vpc_sn_pub_map_public_ip_on_launch}"
+}
 
+resource "aws_subnet" "sn_aws1_priv1" {
+    vpc_id                  = aws_vpc.vpc.id
+    availability_zone       = "${var.vpc_sn_aws1}"
+    cidr_block              = "${var.vpc_sn_aws1_priv1_cidr}"
+}
+
+resource "aws_subnet" "sn_aws2_priv1" {
+    vpc_id                  = aws_vpc.vpc.id
+    availability_zone       = "${var.vpc_sn_aws2}"
+    cidr_block              = "${var.vpc_sn_aws2_priv1_cidr}"
+}
+
+resource "aws_subnet" "sn_aws1_priv2" {
+    vpc_id                  = aws_vpc.vpc.id
+    availability_zone       = "${var.vpc_sn_aws1}"
+    cidr_block              = "${var.vpc_sn_aws1_priv2_cidr}"
+}
+
+resource "aws_subnet" "sn_aws2_priv2" {
+    vpc_id                  = aws_vpc.vpc.id
+    availability_zone       = "${var.vpc_sn_aws2}"
+    cidr_block              = "${var.vpc_sn_aws2_priv2_cidr}"
+}
+
+
+# RESOURCE: ROUTE TABLES FOR THE SUBNETS
+
+resource "aws_route_table" "rt_pub" {
+    vpc_id = aws_vpc.vpc.id
     route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.igw_vpc_iac.id
-    }
-
-    tags = {
-        Name = "vpc_iac_route_table_pub"
+        cidr_block = "${var.vpc_cidr_all}"
+        gateway_id = aws_internet_gateway.igw.id
     }
 }
 
-# Tabela de roteamento Privada
+resource "aws_route_table" "rt_priv" {
+    vpc_id = aws_vpc.vpc.id
+}
 
-resource "aws_route_table" "vpc_iac_route_table_priv" {
-    vpc_id = aws_vpc.vpc_iac.id
 
-    tags = {
-        Name = "vpc_iac_route_table_priv"
+# RESOURCE: ROUTE TABLES ASSOCIATION TO SUBNETS
+
+resource "aws_route_table_association" "rt_pub_sn_aws1_pub" {
+  subnet_id      = aws_subnet.sn_aws1_pub.id
+  route_table_id = aws_route_table.rt_pub.id
+}
+
+resource "aws_route_table_association" "rt_pub_sn_aws2_pub" {
+  subnet_id      = aws_subnet.sn_aws2_pub.id
+  route_table_id = aws_route_table.rt_pub.id
+}
+
+resource "aws_route_table_association" "rt_priv_sn_aws1_priv1" {
+  subnet_id      = aws_subnet.sn_aws1_priv1.id
+  route_table_id = aws_route_table.rt_priv.id
+}
+
+resource "aws_route_table_association" "rt_priv_sn_aws2_priv1" {
+  subnet_id      = aws_subnet.sn_aws2_priv1.id
+  route_table_id = aws_route_table.rt_priv.id
+}
+
+resource "aws_route_table_association" "rt_priv_sn_aws1_priv2" {
+  subnet_id      = aws_subnet.sn_aws1_priv2.id
+  route_table_id = aws_route_table.rt_priv.id
+}
+
+resource "aws_route_table_association" "rt_priv_sn_aws2_priv2" {
+  subnet_id      = aws_subnet.sn_aws2_priv2.id
+  route_table_id = aws_route_table.rt_priv.id
+}
+
+
+# RESOURCE: SECURITY GROUPS
+
+resource "aws_security_group" "vpc_sg_pub" {
+    vpc_id = aws_vpc.vpc.id
+    egress {
+        from_port   = "${var.vpc_sg_port_all}"
+        to_port     = "${var.vpc_sg_port_all}"
+        protocol    = "${var.vpc_sg_protocol_any}"
+        cidr_blocks = ["${var.vpc_cidr_all}"]
+    }
+    ingress {
+        from_port   = "${var.vpc_sg_port_all}"
+        to_port     = "${var.vpc_sg_port_all}"
+        protocol    = "${var.vpc_sg_protocol_any}"
+        cidr_blocks = ["${var.vpc_cidr}"]
+    }
+    ingress {
+        from_port   = "${var.vpc_sg_port_ssh}"
+        to_port     = "${var.vpc_sg_port_ssh}"
+        protocol    = "${var.vpc_sg_protocol_tcp}"
+        cidr_blocks = ["${var.vpc_cidr_all}"]
+    }
+    ingress {
+        from_port   = "${var.vpc_sg_port_http}"
+        to_port     = "${var.vpc_sg_port_http}"
+        protocol    = "${var.vpc_sg_protocol_tcp}"
+        cidr_blocks = ["${var.vpc_cidr_all}"]
     }
 }
 
-
-
-
-
-# Subnet Pública US-EAST-1A
-
-resource "aws_subnet" "sn_vpc_iac_pub_1a" {
-  vpc_id                  = aws_vpc.vpc_iac.id
-  cidr_block              = "${var.sn_vpc_iac_pub_1a_cidr}"
-  map_public_ip_on_launch = "${var.vpc_sn_pub_map_public_ip_on_launch}"
-  availability_zone       = "us-east-1a"
-
-  tags = {
-    "Name" = "sn_vpc_iac_pub_1a"
-  }
+resource "aws_security_group" "vpc_sg_priv" {
+    vpc_id = aws_vpc.vpc.id
+    egress {
+        from_port   = "${var.vpc_sg_port_all}"
+        to_port     = "${var.vpc_sg_port_all}"
+        protocol    = "${var.vpc_sg_protocol_any}"
+        cidr_blocks = ["${var.vpc_cidr_all}"]
+    }
+    ingress {
+        from_port   = "${var.vpc_sg_port_all}"
+        to_port     = "${var.vpc_sg_port_all}"
+        protocol    = "${var.vpc_sg_protocol_any}"
+        cidr_blocks = ["${var.vpc_cidr}"]
+    }
 }
-
-# Associação da Subnet Pública 1A
-resource "aws_route_table_association" "association_pub_1a" {
-  subnet_id      = aws_subnet.sn_vpc_iac_pub_1a.id
-  route_table_id = aws_route_table.vpc_iac_route_table_pub.id
-}
-
-
-
-
-
-# Criação da Subnet Pública US-EAST-1B
-
-resource "aws_subnet" "sn_vpc_iac_pub_1b" {
-  vpc_id                  = aws_vpc.vpc_iac.id
-  cidr_block              = "${var.sn_vpc_iac_pub_1b_cidr}"
-  map_public_ip_on_launch = "${var.vpc_sn_pub_map_public_ip_on_launch}"
-  availability_zone       = "us-east-1b"
-
-  tags = {
-    "Name" = "sn_vpc_iac_pub_1b"
-  }
-}
-
-# Associação da Subnet Pública 1B
-resource "aws_route_table_association" "association_pub_1b" {
-  subnet_id      = aws_subnet.sn_vpc_iac_pub_1b.id
-  route_table_id = aws_route_table.vpc_iac_route_table_pub.id
-}
-
-
-
-
-
-# Criação da Subnet Privada US-EAST-1A
-
-resource "aws_subnet" "sn_vpc_iac_priv_1a" {
-  vpc_id                  = aws_vpc.vpc_iac.id
-  cidr_block              = "${var.sn_vpc_iac_priv_1a_cidr}"
-  availability_zone       = "us-east-1a"
-
-  tags = {
-    "Name" = "sn_vpc_iac_priv_1a"
-  }
-}
-
-# Associação da Subnet Privada 1A
-resource "aws_route_table_association" "association_priv_1a" {
-  subnet_id      = aws_subnet.sn_vpc_iac_priv_1a.id
-  route_table_id = aws_route_table.vpc_iac_route_table_priv.id
-}
-
-
-
-
-
-# Criação da Subnet Privada US-EAST-1B
-
-resource "aws_subnet" "sn_vpc_iac_priv_1b" {
-  vpc_id                  = aws_vpc.vpc_iac.id
-  cidr_block              = "${var.sn_vpc_iac_priv_1b_cidr}"
-  availability_zone       = "us-east-1b"
-
-  tags = {
-    "Name" = "sn_vpc_iac_priv_1b"
-  }
-}
-
-# Associação da Subnet Privada 1B
-resource "aws_route_table_association" "association_priv_1b" {
-  subnet_id      = aws_subnet.sn_vpc_iac_priv_1b.id
-  route_table_id = aws_route_table.vpc_iac_route_table_priv.id
-}
-
-
-# Criação da Subnet Privada 2 US-EAST-1A
-
-resource "aws_subnet" "sn_vpc_iac_priv2_1a" {
-  vpc_id                  = aws_vpc.vpc_iac.id
-  cidr_block              = "${var.sn_vpc_iac_priv2_1a_cidr}"
-  availability_zone       = "us-east-1a"
-
-  tags = {
-    "Name" = "sn_vpc_iac_priv2_1a"
-  }
-}
-
-# Associação da Subnet Privada 1A
-resource "aws_route_table_association" "association_priv2_1a" {
-  subnet_id      = aws_subnet.sn_vpc_iac_priv2_1a.id
-  route_table_id = aws_route_table.vpc_iac_route_table_priv.id
-}
-
-
-
-
-
-# Criação da Subnet Privada 2 US-EAST-1B
-
-resource "aws_subnet" "sn_vpc_iac_priv2_1b" {
-  vpc_id                  = aws_vpc.vpc_iac.id
-  cidr_block              = "${var.sn_vpc_iac_priv2_1b_cidr}"
-  availability_zone       = "us-east-1b"
-
-  tags = {
-    "Name" = "sn_vpc_iac_priv2_1b"
-  }
-}
-
-# Associação da Subnet Privada 1B
-resource "aws_route_table_association" "association_priv2_1b" {
-  subnet_id      = aws_subnet.sn_vpc_iac_priv2_1b.id
-  route_table_id = aws_route_table.vpc_iac_route_table_priv.id
-}
-
-
-
-
-# Criação do Security Group Pública
-
-resource "aws_security_group" "vpc_iac_security_group_pub" {
-  name        = "vpc_iac_security_group_pub"
-  description = "vpc iac Security Group pub"
-  vpc_id      = aws_vpc.vpc_iac.id
-
-  egress {
-      description = "All to All"
-      from_port   = 0
-      to_port     = 0
-      protocol    = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-      description = "All from 10.0.0.0/16"
-      from_port   = 0
-      to_port     = 0
-      protocol    = "-1"
-      cidr_blocks = ["10.0.0.0/16"]
-  }
-
-  ingress {
-      description = "TCP/22 from all"
-      from_port   = 22
-      to_port     = 22
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-      description = "TCP/80 from all"
-      from_port   = 80
-      to_port     = 80
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "vpc iac Security Group pub"
-  }
-}
-
-
-#Security Group privado
-
-resource "aws_security_group" "vpc_iac_security_group_priv" {
-  name        = "vpc_iac_security_group_priv"
-  description = "vpc iac Security Group priv"
-  vpc_id      = aws_vpc.vpc_iac.id
-
-  ingress {
-      description = "All from 10.0.0.0/16"
-      from_port   = 3306
-      to_port     = 3306
-      protocol    = "tcp"
-      cidr_blocks = ["10.0.0.0/16"]
-  }
-
-  egress {
-      description = "All to all"
-      from_port   = 0
-      to_port     = 0
-      protocol    = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
-  }
-
-}
-
--- main.tf
+++ main.tf
@@ -20,8 +20,19 @@ provider "aws" {
                  VPC Modules                
 -------------------------------------------*/
 
-module "VPC" {
-  source  = "./Modules/VPC"
+# MODULES ORCHESTRATOR
+
+module "network" {
+    source                = "./modules/network"
+    vpc_cidr              = "${var.vpc_cidr}"
+    vpc_sn_aws1            = "${var.vpc_sn_aws1}"
+    vpc_sn_aws2            = "${var.vpc_sn_aws2}"
+    vpc_sn_aws1_pub_cidr   = "${var.vpc_sn_aws1_pub_cidr}"
+    vpc_sn_aws2_pub_cidr   = "${var.vpc_sn_aws2_pub_cidr}"
+    vpc_sn_aws1_priv1_cidr = "${var.vpc_sn_aws1_priv1_cidr}"
+    vpc_sn_aws2_priv1_cidr = "${var.vpc_sn_aws2_priv1_cidr}"
+    vpc_sn_aws1_priv2_cidr = "${var.vpc_sn_aws1_priv2_cidr}"
+    vpc_sn_aws2_priv2_cidr = "${var.vpc_sn_aws2_priv2_cidr}"
 }
 
 
